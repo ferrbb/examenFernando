c14739917929b0dc038c16a1466ed24bc98478a6
Fernando Murillo <93768540+ferrbb@users.noreply.github.com>
2022-05-25 11:15:24 +0200

    Create Fernando.java

---
+++ Fernando.java
@@ -0,0 +1,9 @@
+/* Este es un programa simple en Java...
+   NombreArchivo: "Fernando.java". */
+class HelloFernando
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hola Fernando!");
+    }
+}
